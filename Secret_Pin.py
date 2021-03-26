d404a2b003e46e7b320c2ea6caed3dd4f32b1cb5
kimc-OTC <kimc@stu.otc.school.nz>
2021-03-26 23:42:24 +1300

    Added comments.

--- Secret_Pin.py
+++ Secret_Pin.py
@@ -1,8 +1,11 @@
+# set the secret pin as 0456
 secret_pin = int('0456')
 i = 0000
+
+# run the while loop until the pin has been discovered
 while i <= 9999:
     if i == secret_pin:
-        print(f"{i:04d}")
+        print(f"{i:04d}")       # adds a leading zero
         break
     else:
         i += 1
