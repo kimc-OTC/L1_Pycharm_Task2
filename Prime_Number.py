a0b22e6d86f213a6f4a9982bf825677531985d5c
kimc <kimc@stu.otc.school.nz>
2021-03-26 13:28:18 +1300

    Started the extension task. Keep asking user for a number until they type in a prime number.

--- Prime_Number.py
+++ Prime_Number.py
@@ -1,4 +1,12 @@
 user_number = int(input("Type in a number."))
+i = 2
+if user_number > 1:
+    while (user_number % i) == 0:
+        print("{} is not a prime number.".format(user_number))
+        user_number = int(input("Type in another number."))
+    else:
+        print("{} is a prime number.".format(user_number))
+else:
+    print("{} is not a prime number.".format(user_number))
 
-while user_number >= 0:
-    if user_number
+
