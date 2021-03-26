cf74eefa399c6b82f64be254de0eb86abffa9f70
kimc-OTC <kimc@stu.otc.school.nz>
2021-03-26 23:53:56 +1300

    Version with loop.

--- Sum1ton.py
+++ Sum1ton.py
@@ -1,7 +1,13 @@
 # ask the user to pick a number
 n = int(input("Type in a number"))
+sum_numbers = 0
 
 # print the sum of the numbers from 1 to n
-print((1 + n) * n/2)
+for i in range(1, n + 1):
+    sum_numbers = sum_numbers + i
+print(sum_numbers)
+
+# or
+# print((1 + n) * n/2)
 
 
