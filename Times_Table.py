b8c2f178ac8e3f46b67550a952cb3f6a08ba4937
kimc-OTC <kimc@stu.otc.school.nz>
2021-03-26 23:41:51 +1300

    Added comments.

--- Times_Table.py
+++ Times_Table.py
@@ -1,4 +1,5 @@
-for q in range(1,13):
+# prints the times table up to 12 for the first 12 times tables.
+for q in range(1, 13):
     for i in range(1, 13):
         print("{} x {} = {}".format(q, i, i * q))
 
